75ed7014a51fecd2799e1f43924f42a34588ecec
Hwan Jo <nimbusob@gmail.com>
2022-05-31 15:23:02 +0900

    Initial Project Setup

---
+++ app/src/main/kotlin/com/sirloin/mtrace/Main.kt
@@ -0,0 +1,13 @@
+/*
+ * mtrace-api-client
+ * Distributed under MIT licence
+ */
+package com.sirloin.mtrace
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
