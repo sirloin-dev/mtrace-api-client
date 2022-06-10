e6b762b4a6b99253da01f39326f48f28f602ce84
Hwan Jo <nimbusob@gmail.com>
2022-06-10 14:26:39 +0900

    PMD, CheckStyle, SpotBugs 추가

--- app/src/main/java/com/sirloin/mtraceapiclient/internal/http/exception/MtraceHttpConnectException.java
+++ app/src/main/java/com/sirloin/mtraceapiclient/internal/http/exception/MtraceHttpConnectException.java
@@ -1,9 +1,12 @@
 package com.sirloin.mtraceapiclient.internal.http.exception;
 
-
 import javax.validation.constraints.NotNull;
+import java.io.Serial;
 
 public final class MtraceHttpConnectException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = -2338626292552177485L;
+
     public MtraceHttpConnectException(@NotNull String errorMessage, @NotNull Exception originalException) {
         super(errorMessage, originalException);
     }
